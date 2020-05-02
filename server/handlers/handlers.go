0a1560ad2ea2d045f146eb8db52cdf9bb369919a
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-02 00:43:57 -0600

    feat: handle form update

--- server/handlers/handlers.go
+++ server/handlers/handlers.go
@@ -79,37 +79,69 @@ func GetBookHandler(w http.ResponseWriter, r *http.Request) {
 // CreateBookHandler -
 func CreateBookHandler(w http.ResponseWriter, r *http.Request) {
 	db := database.GetDB()
-
+	path := r.URL.Path
 	b := models.Book{}
 
-	err := json.NewDecoder(r.Body).Decode(&b) // parse json request body
+	if contains := strings.Contains(path, "api"); contains {
+
+		err := json.NewDecoder(r.Body).Decode(&b) // parse json request body
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = database.QueryInsertRow(db, b)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		book, err := json.Marshal(b)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(book)
+
+		return
+	}
+
+	// get form data
+	b.Title = r.FormValue("title")
+	b.Author = r.FormValue("author")
+
+	err := database.QueryInsertRow(db, b)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = database.QueryInsertRow(db, b)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	book, err := json.Marshal(b)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(book)
+	http.Redirect(w, r, "/books/", http.StatusMovedPermanently)
 }
 
 // UpdateBookHandler -
 func UpdateBookHandler(w http.ResponseWriter, r *http.Request) {
 	db := database.GetDB()
+	var update models.Book
 
-	update := models.Book{}
+	path := r.URL.Path
 	params := mux.Vars(r)
 	id := params["id"]
 	idInt, _ := strconv.Atoi(id)
+
+	if contains := strings.Contains(path, "api"); !contains {
+		update.Title = r.FormValue("title")
+		update.Author = r.FormValue("author")
+
+		err := database.QueryUpdateRow(db, idInt, "books", update)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		http.Redirect(w, r, "/books/", http.StatusMovedPermanently)
+
+		return
+	}
 
 	err := json.NewDecoder(r.Body).Decode(&update)
 	if err != nil {
@@ -135,6 +167,7 @@ func UpdateBookHandler(w http.ResponseWriter, r *http.Request) {
 func DeleteBookHandler(w http.ResponseWriter, r *http.Request) {
 	db := database.GetDB()
 
+	path := r.URL.Path
 	params := mux.Vars(r)
 	id := params["id"]
 	idInt, _ := strconv.Atoi(id)
@@ -145,7 +178,52 @@ func DeleteBookHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
+	if contains := strings.Contains(path, "api"); !contains {
+		http.Redirect(w, r, "/books/", http.StatusMovedPermanently)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("book successfully deleted..."))
+}
+
+// ShowFormHandler -
+func ShowFormHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := templates.ParseTemplate("./templates/form.tpl")
+	path := r.URL.Path
+
+	if contains := strings.Contains(path, "new"); contains {
+		tmpl.Execute(w, nil)
+		return
+	}
+
+	db := database.GetDB()
+	params := mux.Vars(r)
+	id := params["id"]
+	idInt, _ := strconv.Atoi(id)
+
+	book, err := database.QueryRow(db, "books", idInt)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if contains := strings.Contains(path, "edit"); contains {
+		action := fmt.Sprintf("/books/%s", id)
+		data := struct {
+			Method string
+			Action string
+			Book   models.Book
+		}{
+			Method: "PUT",
+			Action: action,
+			Book:   book,
+		}
+
+		tmpl.Execute(w, data)
+		return
+	}
+
+	// tmpl.Execute(w, nil)
 }
